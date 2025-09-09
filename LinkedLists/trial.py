8009ba8afc55843cb360d17571b55d929a4842d1
ronnyonjetech <nyonjeron@gmail.com>
2025-09-09 21:49:51 +0300

    latest trial

---
+++ LinkedLists/trial.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    
+    def append_last(self,data):
+        node=Node(data)
+        #empty
+        if self.head is None:
+            self.head=node
+            return
+        #non-empty
+        current=self.head
+        while current.next:
+            current=current.next
+        current.next=node
+    
+    def append_first(self,data):
+        node=Node(data)
+        if self.head is None:
+            self.head=node
+            return
+        node.next=self.head
+        self.head=node
+
+    def delete_node(self,data):
+        #empty
+        if self.head is None:
+            print("List is empty")
+            return
+        #at the first node
+        if self.head.data == data:
+           self.head=self.head.next
+           return
+        #non-empty
+        current=self.head
+        while current.next and current.next.data != data:
+            current=current.next
+
+        if current.next is None:
+            print("element not found")
+            return
+
+        current.next=current.next.next
+
+    def delete_middle_node(self):
+      fast=self.head
+      slow=self.head
+      prev=None
+
+      while fast and fast.next:
+        prev=slow
+        slow=slow.next
+        fast=fast.next.next
+      prev.next=slow.next
+      return self.head
+
+
+        
+        
+    def print_list(self):
+        current=self.head
+        while current:
+            print(current.data,end="-->")
+            current=current.next
+        print("None")
+
+
+linkedList=LinkedList()
+linkedList.append_last(200)
+linkedList.append_last(700)
+linkedList.append_last(900)
+linkedList.append_first(10)
+# linkedList.delete_node(10)
+linkedList.delete_middle_node()
+linkedList.print_list()
